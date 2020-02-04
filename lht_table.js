34cdbfb0b5d01205fa793ada4a586abb6464c9a9
Dominic <dtorres243@west-mec.org>
2020-02-03 18:05:26 -0700

    finished the review

--- lht_table.js
+++ lht_table.js
@@ -16,14 +16,20 @@ var thisDay = new Date("August 30, 2018");
 var tableHTML = "<table id='eventTable'><caption>Upcoming Events</caption><tr><th>Date</th><th>Event</th><th>Price</th></tr>";
 //step 6
 var endDate = new Date(thisDay.getTime() + 14 * 24 * 60 * 60 * 1000);
+loop:
 for(var i = 0; i < eventDates.length; i++){
    var eventDate = new Date(eventDates[i]);
-   var eventDay = eventDate.toDateString());
-   var eventTime = eventDate.toLocaleDateString());
+   var eventDay = eventDate.toDateString();
+   var eventTime = eventDate.toLocaleDateString();
 
-   if(thisDay <= eventDate && eventDate <= endDate)
-   
+   if(thisDay <= eventDate && eventDate <= endDate){
+      tableHTML += "<tr><td>" + eventDay + " @ " + eventTime + "</td><td>" + eventDescriptions[i] + "</td><td>" + eventPrices[i] + "</td></tr>";
+   }
 }
+//functions
+tableHTML += "</table>";
+document.getElementById("eventList").innerHTML = tableHTML;
+
 
 
 
