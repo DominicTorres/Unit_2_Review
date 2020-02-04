447225019bce08e6d5956b3ffd85df0100289287
Dominic <dtorres243@west-mec.org>
2020-02-03 17:27:10 -0700

    started unit 2 review

---
+++ lht_table.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Review Assignment
+
+   Author: Dominic Torres
+   Date:  2/3/20 
+
+	
+*/
+//step 4
+var thisDay = new Date("August 30, 2018");
+//step 5
+var tableHTML = "<table id='eventTable'><caption>Upcoming Events</caption><tr><th>Date</th><th>Event</th><th>Price</th></tr>";
+//step 6
+var endDate = new Date(thisDay.getTime() + 14 * 24 * 60 * 60 * 1000);
+for(var i = 0; i < eventDates.length; i++){
+   var eventDate = new Date(eventDates[i]);
+   var eventDay = eventDate.toDateString());
+   var eventTime = eventDate.toLocaleDateString());
+
+   if(thisDay <= eventDate && eventDate <= endDate)
+   
+}
+
+
+
